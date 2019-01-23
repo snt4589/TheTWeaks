ee62a38069ed63d5dea534cc29af739ddc7b7084
Deepak singh <drakerdeepak@gmail.com>
2019-01-24 02:44:11 +0530

    some changes in ui and added few activity

--- app/src/main/java/com/thetweaks/snt/thetweaks/LoginActivity.java
+++ app/src/main/java/com/thetweaks/snt/thetweaks/LoginActivity.java
@@ -20,7 +20,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private EditText email,password,mtxt,fpass,createacc;
     private Button b1;
-    RelativeLayout r1 ,r2;
+
     AnimatorSet animatorSet;
     AnimatorSet animatorSet2;
 
@@ -31,8 +31,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         setContentView(R.layout.activity_login);
 //Initialize elements
-        r1=(RelativeLayout)findViewById(R.id.relativeLayout1);
-        r2=(RelativeLayout)findViewById(R.id.relativeLayout2);
+
         email=(EditText)findViewById(R.id.email);
         password=(EditText)findViewById(R.id.password);
         b1=(Button)findViewById(R.id.login_button);
@@ -49,8 +48,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
 
-       // r1.setBackgroundColor(Color.parseColor("#80000"));
-
+   
 
 
 
@@ -63,33 +61,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
 
             case R.id.email:
-//                r1.setBackgroundColor(Color.parseColor("#62F51F"));
-//                ObjectAnimator fadeout = ObjectAnimator.ofFloat(r1,"alpha",0.5f,0.0f);
-//                fadeout.setDuration(500);
-//
-//                ObjectAnimator fadein=ObjectAnimator.ofFloat(r1,"alpha",0.0f,0.5f);
-//                fadein.setDuration(500);
-//
-//                animatorSet.play(fadein).after(fadeout);
-//
-//                animatorSet.addListener(new AnimatorListenerAdapter() {
-//                    @Override
-//                    public void onAnimationStart(Animator animation, boolean isReverse) {
-//                    animatorSet.setDuration(1);
-//                    }
-//
-//                    @Override
-//                    public void onAnimationEnd(Animator animation) {
-//                        super.onAnimationEnd(animation);
-//                        animatorSet.start();
-//
-//                    }
-//                });
-                //animatorSet.start();
 
-
-                //animatorSet.end();
-              // r1.setBackgroundColor(0);
 
 
 
@@ -97,39 +69,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
 
             case R.id.password:
-//                r2.setBackgroundColor(Color.parseColor("#62F51F"));
-//                ObjectAnimator fadeout1 = ObjectAnimator.ofFloat(r2,"alpha",0.5f,0.0f);
-//                fadeout1.setDuration(500);
-//
-//                ObjectAnimator fadein1=ObjectAnimator.ofFloat(r2,"alpha",0.0f,0.5f);
-//                fadein1.setDuration(500);
-//
-//                animatorSet2.play(fadein1).after(fadeout1);
-//
-//                animatorSet2.addListener(new AnimatorListenerAdapter() {
-//                    @Override
-//                    public void onAnimationStart(Animator animation, boolean isReverse) {
-//                        animatorSet2.setDuration(1);
-//                    }
-//
-//                    @Override
-//                    public void onAnimationEnd(Animator animation) {
-//                        super.onAnimationEnd(animation);
-//
-//                        animatorSet2.start();
-//                    }
-//                });//animatorSet.setCurrentPlayTime(1000);
-//                animatorSet2.start();
-               // r2.setBackgroundColor(0);
 
-                //animatorSet.end();
 
                 break;
 
             case R.id.login_button:
                     b1.setBackgroundResource(R.drawable.btn_afterpressed);
-//                Intent intent = new Intent(this,MainActivity.class);
-//                startActivity(intent);
+
                 break;
 
         }
