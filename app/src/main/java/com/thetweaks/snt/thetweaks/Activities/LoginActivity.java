6fce13622b78d2c93ceff2de6716cfc1330ec001
manigedit <1manish22@gmail.com>
2019-02-21 21:27:36 +0530

    Chat activity

--- app/src/main/java/com/thetweaks/snt/thetweaks/Activities/LoginActivity.java
+++ app/src/main/java/com/thetweaks/snt/thetweaks/Activities/LoginActivity.java
@@ -2,9 +2,9 @@ package com.thetweaks.snt.thetweaks.Activities;
 
 import android.animation.AnimatorSet;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
