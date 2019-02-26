9e6ef9c21ef74616d6976c4e471bfe219487012b
Draker <drakerdeepak@gmail.com>
2019-02-26 22:31:47 +0530

    added publish activity

---
+++ app/src/main/java/com/thetweaks/snt/thetweaks/Publish.java
@@ -0,0 +1,24 @@
+package com.thetweaks.snt.thetweaks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Publish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Select type of your post", "News", "OriginStory","Idea","Opinion","Experience","Thought","Problem","Others"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+}
